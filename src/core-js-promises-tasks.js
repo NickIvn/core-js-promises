1dbb56e736de2d178ebdd84c6d9efadfe6b3bd02
Nikolai <nikolai.ivanov3110@gmail.com>
2024-04-10 12:02:46 +0400

    feat: solve getAllResault, queuePromises tasks

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -117,8 +117,9 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  const alwaysResolvePromises = promises.map((p) => p.catch(() => null));
+  return Promise.all(alwaysResolvePromises);
 }
 
 /**
@@ -139,8 +140,19 @@ function getAllResult(/* promises */) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  let result = '';
+  let chain = Promise.resolve();
+  promises.forEach((promise) => {
+    chain = chain
+      .then(() => promise)
+      .then((value) => {
+        result += value;
+        return result;
+      })
+      .catch(() => {});
+  });
+  return chain.then(() => result);
 }
 
 module.exports = {
